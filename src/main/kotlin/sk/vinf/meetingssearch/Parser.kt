52ef61f0c3fac2d4a5de6b572d7c60d99fddfd1d
Lukas Anda <lukas.anda@gmail.com>
2020-10-25 21:19:07 +0100

    Basic parsing of name + birthdate + deathdate from dump using regex

--- src/main/kotlin/sk/vinf/meetingssearch/Parser.kt
+++ src/main/kotlin/sk/vinf/meetingssearch/Parser.kt
@@ -8,13 +8,16 @@ object Parser {
     fun main(args: Array<String>) {
         val file = File(DATA_PATH)
         file.listFiles()?.forEach {
-            parseFile(it)
+            if (!it.isDirectory) {
+                parseFile(it)
+            }
         }
 
     }
 }
 
 fun parseFile(file: File) {
+    val newFile = File(DATA_PATH + "/parsed/" + file.name.substringBeforeLast(".") + "-infoboxes.txt")
     var processedText = ""
     file.reader().streamXml<String> {
         onTag("text") {
@@ -28,9 +31,69 @@ fun parseFile(file: File) {
             val matcher = infoboxRegex.matcher(processedText)
 
             if (matcher.find()) {
-                println(matcher.group().substring(1..10))
+                val infobox = matcher.group()
+                newFile.appendText(parseInfoBox(infobox))
             }
         }
-        onFinish {}
+        onFinish {
+            ""
+        }
     }
+}
+
+fun parseInfoBox(infoBox: String) = StringBuilder().apply {
+    val person = Person()
+
+    infoBox.split("\n").forEach {
+        when {
+            it.contains(Regex("^\\|\\s*name")) && person.name.isEmpty() -> person.name = it
+            it.contains(Regex("^\\|\\s*birth_date")) && person.birthDate.isEmpty() -> person.birthDate = it
+            it.contains(Regex("^\\|\\s*death_date")) && person.deathDate.isEmpty() -> person.deathDate = it
+        }
+    }
+    if (person.isValid()) {
+        append(person)
+        append("\n")
+    }
+}.toString()
+
+class Person {
+    var name: String = ""
+    var birthDate: String = ""
+    var deathDate: String = ""
+
+    private fun getPersonName(): String {
+        var parsedName = name
+        parsedName = parsedName.substringAfter("=")
+        return parsedName.trim()
+    }
+
+    private fun getBirthDay(): String {
+        //TODO This will later be turned to timestamp
+        var parsedBirthDay = birthDate
+        parsedBirthDay = parsedBirthDay.substringAfter("=")
+        return parsedBirthDay.trim()
+    }
+
+    private fun getDeathDay(): String {
+        //TODO This will later be turned to timestamp
+        var parsedDeathDate = deathDate
+        parsedDeathDate = parsedDeathDate.substringAfter("=")
+        return parsedDeathDate.trim()
+    }
+
+    fun isValid() = getPersonName().isNotEmpty() && getBirthDay().isNotEmpty()
+
+    override fun toString() = StringBuilder().apply {
+        append(getPersonName())
+
+        val birthDay = getBirthDay()
+        append(" | ")
+        append(birthDay)
+
+
+        val deathDay = getDeathDay()
+        append(" | ")
+        append(deathDay)
+    }.toString()
 }
